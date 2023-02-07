db783f906c52af40f829e107b1815307c76ddbf9
Brad Bomkamp <37642026+bbomkamp@users.noreply.github.com>
2023-02-07 16:15:47 -0600

    getPost is working

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 import Link from "next/link"
 
 
-export default function Home() {
+async function getPosts(){
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
+  if(!res.ok){
+    console.log(res)
+  }
+  return res.json()
+}
+
+export default async function Home() {
+
+  const data : { id: number; title: string }[]= await getPosts()
+  console.log(data)
   return (
     <main className="py-4 px-48">
-      <Link className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md" href={"/dashboard"}>
+      <Link 
+      className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md" 
+      href={"/dashboard"}
+      >
         Goto The Dashboard
       </Link>
+
+      {data.map((post) => (
+        <h1 className="text-lg py-6">{post.title}</h1>
+      ))}
     </main>
   )
 }
